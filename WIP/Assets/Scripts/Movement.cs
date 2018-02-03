7cbebae05698385f3ea2c47bca794aa4ac82cfec
AmenoAmeno <35885159+AmenoAmeno@users.noreply.github.com>
2018-02-02 21:16:07 -0500

    AddedJump
    Jump is jank. It has build up time to get to the height, and you can jump multiple times in the air. Need to solve that

--- WIP/Assets/Scripts/Movement.cs
+++ WIP/Assets/Scripts/Movement.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public GameObject character;
+
 	public float speed;
+	public float jumpSpeed;
 	// Use this for initialization
 	void Start () {
-		speed = 6.0f;
+		//start speed of the Char
+		speed = 4.0f;
+		jumpSpeed = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		//gets the input from D to move the Char to the RIGHT
 		if (Input.GetKey(KeyCode.D)){
 			character.transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		//gets the input from A to move the Char to the LEFT
 		if (Input.GetKey(KeyCode.A)){
 			character.transform.position += Vector3.left* speed * Time.deltaTime;
 		}
+		//gets the input from W to move the Char UP
 		if (Input.GetKey(KeyCode.W)){
 			character.transform.position += Vector3.forward * speed * Time.deltaTime;
 		}
+		//gets the input from S to move the Char DOWN
 		if (Input.GetKey(KeyCode.S)){
 			character.transform.position += Vector3.back* speed * Time.deltaTime;
 		}
+		if (Input.GetKey (KeyCode.Space)) {
+			character.transform.position += Vector3.up* jumpSpeed* Time.deltaTime;
+		}
+	}
+	void FixedUpdate(){
+		
 	}
 }
