de861a886c76012a11e8e7db456e1c6f68a167d5
AmenoAmeno <35885159+AmenoAmeno@users.noreply.github.com>
2018-02-06 18:54:22 -0500

    PointClick
    Moves Char to position. Needs to have a time to position put in.

---
+++ WIP/Assets/ClipMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipMovement : MonoBehaviour {
+	public GameObject Char;
+	public GameObject WayPoint;
+
+	public float Speed;
+	Vector3 targetPosition;
+
+	void Start () {
+
+		targetPosition = transform.position;
+		Speed = 3.0f;
+	}
+	void Update ()
+	{
+		//float step = Speed * Time.deltaTime;
+
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				targetPosition = hit.point;
+				WayPoint.transform.position = targetPosition;
+				Char.transform.position = Vector3.Lerp (transform.position, targetPosition, Speed);
+			}
+		}
+	}
+}
