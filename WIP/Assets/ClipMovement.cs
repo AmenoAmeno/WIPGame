f5c773f8496111286a9dd44f338441e5683b5574
AmenoAmeno <35885159+AmenoAmeno@users.noreply.github.com>
2018-02-06 21:34:48 -0500

    MayHaveBrokeEveything
    Took a Step Back to Take a Step Forward. Point to a spot, and spawn a waypoint. Char doesnt Move to point but code is in there in the works

--- WIP/Assets/ClipMovement.cs
+++ WIP/Assets/ClipMovement.cs
@@ -2,31 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClipMovement : MonoBehaviour {
-	public GameObject Char;
-	public GameObject WayPoint;
+public class ClipMovement : MonoBehaviour
+{
+    public GameObject Char;
+    public GameObject Prefab_WayPoint;
 
-	public float Speed;
-	Vector3 targetPosition;
+    public float Speed;
+    
+    Vector3 targetPosition;
+    Vector3 CharCurrentPos;
+    //Vector3 CharNewPos;
 
-	void Start () {
+    void Start()
+    {
+        //CharCurrentPos = transform.position;
+        targetPosition = transform.position;
+        Speed = .9f;
+    }
+    void Update()
+    {
+       
 
-		targetPosition = transform.position;
-		Speed = 3.0f;
-	}
-	void Update ()
-	{
-		//float step = Speed * Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-		if (Input.GetMouseButtonDown (0)) {
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                targetPosition = hit.point;
+                //CharNewPos = targetPosition;
+               
+                Instantiate(Prefab_WayPoint, hit.point, Quaternion.identity);
 
-			if (Physics.Raycast (ray, out hit)) {
-				targetPosition = hit.point;
-				WayPoint.transform.position = targetPosition;
-				Char.transform.position = Vector3.Lerp (transform.position, targetPosition, Speed);
-			}
-		}
-	}
+               // GameObject myObject = GameObject.FindGameObjectWithTag("Char");
+               // myObject.transform.position = CharCurrentPos;
+               // CharNewPos = Vector3.Lerp(CharCurrentPos, targetPosition, Speed);
+
+                //myObject.transform.position = CharNewPos;
+            }
+
+
+        }
+    }
 }
